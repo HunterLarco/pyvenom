34460624d3ef25fb8078e68b3c0106d4dd871182
Hunter John Larco <admin@larcolabs.com>
2016-03-01 00:23:53 -0800

    Basic Script Execution and Console

--- example/venom/__init__.py
+++ example/venom/__init__.py
@@ -5,6 +5,7 @@ from google.appengine.ext.webapp import template
 import webapp2
 import os
 import mimetypes
+import json
 
 
 class ResourceHandler(webapp2.RequestHandler):
@@ -27,18 +28,45 @@ class MainHandler(webapp2.RequestHandler):
       self.redirect(self.request.path + '/')
       return
     
-    script_names = map(lambda x: x.__name__, scripts)
-    
     template_values = {
-      'scripts': script_names
+      'scripts': scripts.keys()
     }
     path = os.path.join(os.path.dirname(__file__), 'templates/scripts.html')
     self.response.out.write(template.render(path, template_values))
 
 
+from cStringIO import StringIO
+import sys
+
+class Capturing(list):
+  def __enter__(self):
+    self._stdout = sys.stdout
+    sys.stdout = self._stringio = StringIO()
+    return self
+  
+  def __exit__(self, *args):
+    self.extend(self._stringio.getvalue().splitlines())
+    self._stringio.truncate(0)
+    sys.stdout = self._stdout
+
+
+class ScriptExecutionHandler(webapp2.RequestHandler):
+  def post(self, script_name):
+    if not script_name in scripts:
+      self.error(500)
+    script = scripts[script_name]
+    with Capturing() as printed:
+      returned = script()
+    self.response.out.write(json.dumps({
+      'printed': printed,
+      'returned': returned
+    }))
+
+
 ui = webapp2.WSGIApplication([
   ('.*/(resources|images)/(.*)', ResourceHandler),
   ('.*/(scripts|endpoints|models|scaffold)', PageHandler),
+  ('.*/scripts/execute/([^/]+)/?', ScriptExecutionHandler),
   ('.*', MainHandler)
 ], debug=True)
 
@@ -49,10 +77,10 @@ ui = webapp2.WSGIApplication([
 
 
 
-scripts = []
+scripts = {}
 
 def script(funct):
-  scripts.append(funct)
+  scripts[funct.__name__] = funct
   return funct
   
   

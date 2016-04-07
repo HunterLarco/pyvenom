9bfa54563c468df8bd443a447bacd59d9d0c7c57
Hunter John Larco <admin@larcolabs.com>
2016-04-07 12:33:37 -0700

    Properties with arguments ideation

--- venom_ideation3.py
+++ venom_ideation3.py
@@ -4,20 +4,32 @@ import venom
 app = venom.Application(version=1, debug=True, protocol=venom.Protocols.JSONProtocol)
 
 
+# migration
 class User(venom.Model):
-  username = venom.Properties.String(min=1)
-  lastname = venom.Properties.String(min=1)
+  username  = venom.Properties.String(min=1)
+  lastname  = venom.Properties.String(min=1)
   firstname = venom.Properties.String(min=1)
-  age  = venom.Properties.Int(min=0)
+  age       = venom.Properties.Int(min=0)
+  location  = venom.Properties.Geopoint()
   
   adults = venom.Query(age >= 18)
   children = venom.Query(age < 18)
+  
+  byage = venom.QueryString(age, username)
+  
+  olderthan = venom.Query(age > venom.QueryProperty)
+  nearme = venom.Query(location.distance(venom.QueryProperty('latlon')) < 1000)
   
 
 app.serve('/users/adults', User.adults)
 
 
 app.CRUD('/users', User)
+
+
+
+User.nearme(latlong=(45, 78))
+User.olderthan(21)
 
 
 class QueryHander(venom.RequestHandler):

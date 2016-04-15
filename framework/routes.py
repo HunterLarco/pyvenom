a4e8bf7750088714b66129acac7f42ce79c37029
Hunter John Larco <admin@larcolabs.com>
2016-04-15 11:25:20 -0700

    Added UI elements to example project

--- framework/routes.py
+++ framework/routes.py
@@ -12,16 +12,19 @@ from models import User
 version1 = venom.Application(version=1)
 
 
+venom.ui(
 version1.POST('/login', LoginHandler).body({
   'username': venom.Parameters.String(),
   'password': venom.Parameters.String()
-})
+}), 1000)
 
+
+venom.ui(
 version1.POST('/signup', SignupHandler).body({
   'username': venom.Parameters.String(),
   'password': venom.Parameters.String(),
   'age': venom.Parameters.Integer(required=False, min=0, max=200)
-})
+}), 2000)
 
 # right now this has an error where it created
 # a new user instead of updating the old one

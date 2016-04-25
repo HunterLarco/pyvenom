89a92e7b2109a6be4e7a9882b502ceae92cb4c27
Hunter John Larco <admin@larcolabs.com>
2016-04-25 14:57:09 -0700

    Proof-of-concept User Auth

--- framework/app.py
+++ framework/app.py
@@ -5,40 +5,58 @@ app = venom.Application()
 
 
 class User(venom.Model):
-  age = venom.Properties.Integer(min=0)
   password = venom.Properties.Password(min=3, max=100, required=True)
-  username = venom.Properties.String(min=3, max=100, required=True)
+  username = venom.Properties.String  (min=3, max=100, required=True, unique=True)
+  email    = venom.Properties.String  (min=3, max=100, required=True, unique=True)
+  
   
   by_username = venom.Query(username == venom.QP)
+  by_email = venom.Query(email == venom.QP)
+  
+  login_by_username = venom.Query(by_username, password == venom.QP)
+  login_by_email = venom.Query(by_email, password == venom.QP)
+
+class SessionToken(venom.Model):
+  token = venom.Properties.UUID()
+  user = venom.Properties.Model(User)
+  
+  by_token = venom.Query(token == venom.QP)
 
 
-app.CRUD('/users', User)
+class UserGenericHandler(venom.RequestHandler):
+  def post(self):
+    username, email, password = self.body.get('username', 'email', 'password')
+    
+    user = User(email=email, password=password, username=username).save()
+    auth = SessionToken(user=user).save()
+
+    return dict(user.__json__().items() + [('session_token', auth.token)])
 
 
-class Thing(venom.RequestHandler):
+class UserAuthHandler(venom.RequestHandler):
   def get(self):
-    pass
+    auth = self.headers.get('x-session')
+    return auth.user
 
 
-app.GET('/thing/:foo/list/:bar', Thing).url({
-  'foo': venom.Parameters.String(min=1),
-  'bar': venom.Parameters.String(min=1)
-}).query({
-  'query_int': venom.Parameters.Integer(min=1, required=False, choices=[1, 2, 3, 5, 8]),
-  'query_float': venom.Parameters.Float(min=1, required=False)
-}).headers({
-  'X-Authorization': venom.Parameters.Model(User)
-}).body({
-  'numbers_list': venom.Parameters.List(
-    venom.Parameters.Float(min=-10, max=10)
-  , required=False),
-  'integer': venom.Parameters.Integer(),
-  'thing': {
-    'foo': venom.Parameters.Float(),
-    'bar': {
-      'things': venom.Parameters.List({
-        'name': venom.Parameters.String()
-      })
-    }
-  }
+class UserAuthParameter(venom.Parameters.Model):
+  def __init__(self, required=True):
+    super(UserAuthParameter, self).__init__(SessionToken, required=required)
+  
+  def cast(self, key, value):
+    value = super(venom.Parameters.Model, self).cast(key, value)
+    results = SessionToken.by_token(value)
+    if not results:
+      return None
+    return results[0]
+
+
+app.POST('/users', UserGenericHandler).body({
+  'username': User.username.to_route_parameter(),
+  'email'   : User.email.to_route_parameter(),
+  'password': User.password.to_route_parameter()
+})
+
+app.GET('/auth', UserAuthHandler).headers({
+  'X-Session': UserAuthParameter()
 })

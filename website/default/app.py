7e2c9ffd903f52d748b710af5314f8f29fa1489b
Hunter John Larco <admin@larcolabs.com>
2016-04-28 11:53:50 -0700

    Boilerplate Landing Page Setup

--- website/default/app.py
+++ website/default/app.py
@@ -1,9 +1,13 @@
+from google.appengine.ext.webapp import template
 import webapp2
+import os
 
 
 class DefaultHandler(webapp2.RequestHandler):
   def get(self):
-    self.response.write('test default')
+    template_values = {}
+    path = os.path.join(os.path.dirname(__file__), 'templates/landing.html')
+    self.response.out.write(template.render(path, template_values))
 
 
 app = webapp2.WSGIApplication([

5d09c1e049a24616d2909f58502c4672a98bb011
Hunter John Larco <admin@larcolabs.com>
2016-04-27 22:11:37 -0700

    Ideation for CRUD Domain

--- example/app.py
+++ example/app.py
@@ -1,7 +1,16 @@
 import venom
 
 
-app = venom.Application()
+app = venom.Application(packages=venom.User, default_auth='X-Authorization')
 
 
-import user
+class TodoList(venom.Model):
+  name = venom.Properties.String(min=1)
+
+class Todo(venom.Model):
+  name = venom.Properties.String(min=1)
+  belongs_to = TodoList
+
+
+app.CRUD('/lists', TodoList).auth(venom.User)
+app.CRUD('/lists/:todolist/todo', Todo).auth(venom.User)
